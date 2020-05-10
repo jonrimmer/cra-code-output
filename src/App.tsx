73c21abaf52792636e8632ee6c6610212dac7616
Jon Rimmer <jon.rimmer@gmail.com>
2020-05-10 10:52:27 +0100

    use private field

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class Store {
+  #something: string;
+
+  constructor(something: string) {
+    this.#something = something;
+  }
+
+  get something() {
+    return this.#something;
+  }
+}
 
 function App() {
+  const [store, setStore] = useState(() => new Store("something"));
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload. {store.something}
         </p>
         <a
           className="App-link"
